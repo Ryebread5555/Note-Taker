b72f9e13d230c7e9c2133412b17759b52e996bd3
Ryebread5555 <Rypetty55@gmail.com>
2023-02-12 14:16:50 -0600

    removed notes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,12 +3,11 @@ const path = require('path');
 // routing
 module.exports = (app) => {
     
-    //TODO: create routes
-    //GET /notes will return the notes.html file
+
     app.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
-    //TODO: GET * to return the index.html
+
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
