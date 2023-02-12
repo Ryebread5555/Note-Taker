106a716212b02ea37c4d526cbdf64a9f2f8c63ed
Ryebread5555 <Rypetty55@gmail.com>
2023-02-12 12:13:17 -0600

    created POST for notes and push into json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,4 @@
+// create dependencies
 const path = require('path');
 const fs = require('fs');
 
@@ -12,3 +13,22 @@ module.exports = (app) => {
     });
     
 }
+
+// TODO: Create POST /api/notes, add it to db.json file
+app.post('./api/notes', (req, res) => {
+    const db = fs.readFileSync('db/db.json');
+    db = JSON.parse(db);
+    res.json(db);
+
+    const newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uniqId(),
+    };
+    // TODO: Push newNote into db.json 
+    db.push(newNote);
+    fs.writeFileSync('db/db.json', JSON.stringify(db));
+    res.json(db);
+});
+
+// TODO: Create DELETE /api/notes with query parameter containing the id of the note to delete.
